9b754600338e5c1700282417bf5641692bbbc1b9
Mhmd-Elawady <mhmdelmetwally22@gmail.com>
2025-11-20 17:21:55 +0200

    Add digitize kata solution

---
+++ Convert-number-to-reversed-array.js
@@ -0,0 +1,39 @@
+// ================================================
+// Task:
+// Given a non-negative number, return its digits
+// inside an array in reverse order.
+//
+// Example:
+//   35231 => [1, 3, 2, 5, 3]
+//   0     => [0]
+// ================================================
+
+
+// ---------------------
+// Solution 1 (String + split + reverse + map):
+// ---------------------
+function digitize(n) {
+    return String(n).split("").reverse().map(Number);
+}
+
+
+// ---------------------
+// Solution 2 (loop):
+// ---------------------
+function digitize(n) {
+    let result = [];
+    while (n > 0) {
+        result.push(n % 10); 
+        n = Math.floor(n / 10);
+    }
+
+    if (result.length === 0) return [0]; 
+    return result;
+}
+
+
+// ---------------------
+// Test
+// ---------------------
+console.log(digitize(35231)); // [1, 3, 2, 5, 3]
+console.log(digitize(0));     // [0]
