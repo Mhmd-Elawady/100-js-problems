102289c807d82ec810b8372e30d063b14f82bbec
Mhmd-Elawady <mhmdelmetwally22@gmail.com>
2025-11-21 18:17:18 +0200

    Add opposite number task in math problems section

---
+++ math/Opposite_number.js
@@ -0,0 +1,19 @@
+// ================================================
+// Task:
+// Given a number, return its opposite (additive inverse).
+// Examples:
+//   1  => -1
+//   14 => -14
+//   -34 => 34
+// ================================================
+
+function opposite(number) {
+  return -number;
+}
+
+// ---------------------
+// Test
+// ---------------------
+console.log(opposite(1));   // -1
+console.log(opposite(14));  // -14
+console.log(opposite(-34)); // 34
