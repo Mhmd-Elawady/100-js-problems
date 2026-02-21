4246b4a8338b5862b9287eea78cf60a6da8830b7
Mhmd-Elawady <mhmdelmetwally22@gmail.com>
2026-02-21 23:24:05 +0200

    Add Codewars 8kyu solution: remove spaces from string

---
+++ string/Remove-String-Spaces.js
@@ -0,0 +1,23 @@
+// Codewars Kata 8
+// Task:
+// Write a function that removes all spaces from a string
+// and returns the resulting string.
+
+// Examples:
+// "8 j 8   mBliB8g  imjB8B8  jl  B" 
+// => "8j8mBliB8gimjB8B8jlB"
+//
+// "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"
+// => "88Bifk8hB8BB8BBBB888chl8BhBfd"
+//
+// "8aaaaa dddd r     "
+// => "8aaaaaddddr"
+
+function noSpace(x) {
+    return x.replaceAll(" ", "");
+}
+
+// ================================================
+// Test
+// ================================================
+console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
