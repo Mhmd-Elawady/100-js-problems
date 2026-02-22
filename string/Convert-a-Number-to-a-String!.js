88c4aa4b5a378d43d37866aa806d959e3d80e4fe
Mhmd-Elawady <mhmdelmetwally22@gmail.com>
2026-02-22 04:18:13 +0200

    Add Codewars 8kyu solution: convert number to string

---
+++ string/Convert-a-Number-to-a-String!.js
@@ -0,0 +1,17 @@
+// Codewars Kata 8
+// Task:
+// Create a function that converts a number into a string.
+//
+// Examples:
+// 123  => "123"
+// 999  => "999"
+// -100 => "-100"
+
+function numberToString(num) {
+    return num.toString();
+}
+
+// ================================================
+// Test
+// ================================================
+console.log(numberToString(145));   // "145"
