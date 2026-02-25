59ce5f55c595c02089cb4bc4b57257981b1874f4
Mhmd-Elawady <mhmdelmetwally22@gmail.com>
2026-02-25 23:54:51 +0200

    Add Codewars 8kyu solution: convert string to number

---
+++ string/Convert-a-String-to-a-Number!.js
@@ -0,0 +1,16 @@
+// Codewars Kata 8
+// Task:
+// Convert a string to a number.
+//
+// Examples:
+// "1235" => 1235
+// "-7"   => -7
+
+function stringToNumber(str) {
+    return Number(str);
+}
+
+// ================================================
+// Test
+// ================================================
+console.log(stringToNumber("1235")); // 1235
